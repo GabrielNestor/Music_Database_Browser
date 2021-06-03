d0363fbcb44896cecea3c9c3c08f091a14cb458f
Nestor Lucian <lucian.nestor@yahoo.com>
2021-06-03 21:53:27 +0300

    Revert "Revert "Creating a Scrollbox class for the scrollable listboxes""
    This reverts commit 723a7a5ccb86bf8c20b23086d0de0bf4121e5b48.

--- Jukebox.py
+++ Jukebox.py
@@ -3,6 +3,7 @@ import tkinter
 
 conn = sqlite3.connect("music.sqlite")
 
+<<<<<<< HEAD
 
 class Scrollbox(tkinter.Listbox):
     def __init__(self, window, **kwargs):
@@ -14,6 +15,8 @@ class Scrollbox(tkinter.Listbox):
         self.scrollbar.grid(row=row, column=column, sticky="nsw", rowspan=rowspan)
         self["yscrollcommand"]=self.scrollbar.set
 
+=======
+>>>>>>> parent of 05c5ae4 (We are creating a class that includes it's own scrollbar. It will inherit most of it's functionality.)
 mainWindow = tkinter.Tk()
 mainWindow.title("Music DB Browser")
 mainWindow.geometry("1024x768")
@@ -34,25 +37,21 @@ tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
 tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
 # Artist Listbox
-artistList = Scrollbox(mainWindow)
+artistList = tkinter.Listbox(mainWindow)
 artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artistList.config(border=2, relief='sunken')
-
-# artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
-# artistScroll.grid(row=1, column=0, sticky="nse", rowspan=2)
-# artistList["yscrollcommand"] = artistScroll.set
 
 # Album Listbox
 albumLV = tkinter.Variable(mainWindow)
 albumLV.set(("Choose an artist",))
-albumList = Scrollbox(mainWindow, listvariable=albumLV)
+albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)
 albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 albumList.config(border=2, relief='sunken')
 
 # Song Listbox
 songLV = tkinter.Variable(mainWindow)
-songLV.set(("Choose an album",))
-songList = Scrollbox(mainWindow, listvariable=songLV)
+albumLV.set(("Choose an album",))
+songList = tkinter.Listbox(mainWindow, listvariable=songLV)
 songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 songList.config(border=2, relief='sunken')
 
