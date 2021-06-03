2fd17cfadd7022af0e930e1a1c94d027a228b175
Nestor Lucian <lucian.nestor@yahoo.com>
2021-06-03 21:35:24 +0300

    Creating a Scrollbox class for the scrollable listboxes

--- Jukebox.py
+++ Jukebox.py
@@ -3,6 +3,16 @@ import tkinter
 
 conn = sqlite3.connect("music.sqlite")
 
+class Scrollbox(tkinter.Listbox):
+    def __init__(self, window, **kwargs):
+        super().__init__(window, **kwargs)
+
+        self.scrollbar = tkinter.Scrollbar(window, orient=tkinter.VERTICAL, command=self.yview)
+    def grid(self, row, column, sticky="nsw", rowspan=1, columnspan=1, **kwargs):
+        super().grid(row=row, column=column, sticky=sticky, rowspan=rowspan, columnspan=columnspan, **kwargs)
+        self.scrollbar.grid(row=row, column=column, sticky="nsw", rowspan=rowspan)
+        self["yscrollcommand"]=self.scrollbar.set
+
 mainWindow = tkinter.Tk()
 mainWindow.title("Music DB Browser")
 mainWindow.geometry("1024x768")
@@ -23,21 +33,25 @@ tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
 tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
 # Artist Listbox
-artistList = tkinter.Listbox(mainWindow)
+artistList = Scrollbox(mainWindow)
 artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artistList.config(border=2, relief='sunken')
+
+# artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
+# artistScroll.grid(row=1, column=0, sticky="nse", rowspan=2)
+# artistList["yscrollcommand"] = artistScroll.set
 
 # Album Listbox
 albumLV = tkinter.Variable(mainWindow)
 albumLV.set(("Choose an artist",))
-albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)
+albumList = Scrollbox(mainWindow, listvariable=albumLV)
 albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 albumList.config(border=2, relief='sunken')
 
 # Song Listbox
 songLV = tkinter.Variable(mainWindow)
-albumLV.set(("Choose an album",))
-songList = tkinter.Listbox(mainWindow, listvariable=songLV)
+songLV.set(("Choose an album",))
+songList = Scrollbox(mainWindow, listvariable=songLV)
 songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 songList.config(border=2, relief='sunken')
 
